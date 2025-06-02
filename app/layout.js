a5cc88ff7f78cf90259bb3f53440449de656c717
Meakpiseth <meakpisethmm30@gmail.com>
2025-06-02 16:21:55 +0700

    commit

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
 
 export const metadata = {
-  title: "QuickCart - GreatStack",
+  title: "Tech3 Stores",
   description: "E-Commerce with Next.js ",
 };
 
