509378efb3234fcf8f2a7601cd09c252b64a760f
Meakpiseth <meakpisethmm30@gmail.com>
2025-06-22 21:02:33 +0700

    commit

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,18 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+export default clerkMiddleware((auth, req) => {
+
+  try {
+    return;
+  } catch (error) {
+    console.error('Clerk middleware error:', error);
+    return;
+  }
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
+    '/((?!_next/static|_next/image|favicon.ico).*)',
     '/(api|trpc)(.*)',
   ],
 };
