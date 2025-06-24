03d6bcd41f557adbe10c97c4c12adf0dbed0afcf
Meakpiseth <meakpisethmm30@gmail.com>
2025-06-25 00:27:55 +0700

    commit

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,9 @@
-// middleware.ts
 import { clerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
 
-export default clerkMiddleware((auth, req) => {
-  return NextResponse.next();
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
-
     "/((?!_next/static|_next/image|favicon.ico).*)",
   ],
 };
